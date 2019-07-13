c55509b93998f57f7da760a9a6aa153fad0ac08d
alejovillegas7 <alejovillegaslopez@gmail.com>
2019-07-13 09:31:25 -0500

    Dojo Ionic

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Camera as camera} from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  foto:any;
+
+  constructor(private camera) {}
+
+  capturarFoto(){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.destinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(dataImage =>{
+      this.foto='data:image/jpeg;base64,'+dataImage;
+    })
+  }
+
+  buscarFoto(){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.destinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+
+    }).then(dataImage =>{
+      this.foto='data:image/jpeg;base64,'+dataImage;
+    })
+  }
 
 }
